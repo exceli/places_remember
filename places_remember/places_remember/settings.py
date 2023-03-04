4d7701594edebc905d244573c01db046a8131188
excel <danil_gorokhov@list.ru>
2023-03-05 00:30:02 +0500

    postgres connection

--- places_remember/places_remember/settings.py
+++ places_remember/places_remember/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = "places_remember.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'places_remember',
+        'USER': 'postgres',
+        'PASSWORD': 'DonyaPasswordPostgreSQL',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
